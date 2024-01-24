b72bcab7df7340859462ca2bdca75e18a1e9494c
theRightWay77 <Modnicazd77@gmail.com>
2024-01-24 11:18:10 +0300

    сделано задание 4 из домашки 4.2 с защитой от дурака. Можно было еще добавить NumberFormatInfo для пользователя, который целую часть числа отделяет точкой, а не запятой

---
+++ WebApplication1/Controllers/CalcController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: CalculatorController
+        public string Index(string a, string b, string c)
+        {
+            if(! double.TryParse(a ?? "0", out double num1) || !double.TryParse(b ?? "0", out double num2))
+                { return "Неверный формат введенных данный, будьте внимательней"; }
+            if(! char.TryParse(c ?? "+", out var operat))
+                { return "Такого оператора не существует, или его нельзя передать"; }
+
+            switch (c)
+            {
+                case "+": return $"{a} {c} {b} = {num1 + num2}";
+                case "-": return $"{a} {c} {b} = {num1 - num2}";
+                case "*": return $"{a} {c} {b} = {num1 * num2}";
+                case "/": return $"{a} {c} {b} = {num1 / num2}";
+                default: return "Такого оператора не существует, можно использовать +,-,*,/";
+            }
+
+         
+        }
+
+       
+
+    }
+}
