2dd4883e5c5a37f74dc8a285f5adc744a0f5ffd0
theRightWay77 <Modnicazd77@gmail.com>
2024-01-27 12:01:24 +0300

    разобралась где и как делать визуал, оформление карточек товаров, bootstrap

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -7,20 +7,20 @@ namespace WebApplication1.Models
         private static int IdCreator = 0;
         public int Id; // идентификатор товара.Нужно для уникальности объектов.
         public string Name; // название товара. 
-        public int Сost; // стоимость товара.
+        public int Cost; // стоимость товара.
         public string Description;// подробное описание товара
 
         public Product(string name, int сost, string description)
         {
             Id = IdCreator++;
             Name = name;
-            Сost = сost;
+            Cost = сost;
             Description = description;
         }
 
         public override string ToString()
         {
-            return $"ID: {this.Id}\n{this.Name}\n{this.Сost} p.\n{this.Description}";
+            return $"ID: {this.Id}\n{this.Name}\n{this.Cost} p.\n{this.Description}";
         }
     }
 }
